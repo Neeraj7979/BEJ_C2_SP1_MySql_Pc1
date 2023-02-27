f5873aed28f68797753a1acdd2586a46800356bf
Neeraj7979 <Imnsinha77@outlook.com>
2023-02-27 15:22:25 +0530

    Created fields for Product class and decorated class by @Entity and primary key by @Id

--- src/main/java/com/niit/bej/c2s1MysqlPc1/domain/Product.java
+++ src/main/java/com/niit/bej/c2s1MysqlPc1/domain/Product.java
@@ -1,4 +1,13 @@
 package com.niit.bej.c2s1MysqlPc1.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Product {
+    @Id
+    private int productId;
+    private String productName;
+    private String productDescription;
+    private int amountInStock;
 }
