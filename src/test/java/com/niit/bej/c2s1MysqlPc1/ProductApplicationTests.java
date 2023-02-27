a5986ace202802fb78537e73320573bc417ebf38
Neeraj7979 <Imnsinha77@outlook.com>
2023-02-27 15:16:25 +0530

    Initial commit

---
+++ src/test/java/com/niit/bej/c2s1MysqlPc1/ProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niit.bej.c2s1MysqlPc1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
