22cf0c6663bf333ba03d42e65820a02adf92469a
Neeraj7979 <Imnsinha77@outlook.com>
2023-02-27 15:34:28 +0530

    Created interface ProductRepository which extends crudRepository

---
+++ src/main/java/com/niit/bej/c2s1MysqlPc1/repositry/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.niit.bej.c2s1MysqlPc1.repositry;
+
+import com.niit.bej.c2s1MysqlPc1.domain.Product;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProductRepository extends CrudRepository<Product, Integer> {
+
+}
