395f5e5c7cda173c55937142e1fced62932695ef
Neeraj7979 <Imnsinha77@outlook.com>
2023-02-27 16:31:35 +0530

    Created delete mapping method to add new the product

--- src/main/java/com/niit/bej/c2s1MysqlPc1/controller/ProductController.java
+++ src/main/java/com/niit/bej/c2s1MysqlPc1/controller/ProductController.java
@@ -5,10 +5,7 @@ import com.niit.bej.c2s1MysqlPc1.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,19 +21,24 @@ public class ProductController {
     @GetMapping("/fetchAllProduct")
     public ResponseEntity<?> getAllProduct() {
         List<Product> productList = productService.getAllProduct();
-        if (productList.isEmpty())
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        else
-            return new ResponseEntity<>(productList, HttpStatus.FOUND);
+        if (productList.isEmpty()) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        else return new ResponseEntity<>(productList, HttpStatus.FOUND);
     }
 
     @PostMapping("/addNewProduct")
     public ResponseEntity<?> addNewProduct(@RequestBody Product product) {
         Product newProduct = productService.addProduct(product);
-        if (newProduct != null)
-            return new ResponseEntity<>(newProduct, HttpStatus.CREATED);
+        if (newProduct != null) return new ResponseEntity<>(newProduct, HttpStatus.CREATED);
+        else return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
+
+    }
+
+    @DeleteMapping("/deleteProduct")
+    public ResponseEntity<?> deleteProduct(@PathVariable Integer id) {
+        boolean status = productService.deleteProductById(id);
+        if (status)
+            return new ResponseEntity<>(HttpStatus.ACCEPTED);
         else
             return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
-
     }
 }
