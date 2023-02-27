ffd68e858632d511dd2ff68efb638622b5bbef71
Neeraj7979 <Imnsinha77@outlook.com>
2023-02-27 17:54:19 +0530

    Final commit

--- src/main/java/com/niit/bej/c2s1MysqlPc1/repositry/ProductRepository.java
+++ src/main/java/com/niit/bej/c2s1MysqlPc1/repositry/ProductRepository.java
@@ -2,7 +2,9 @@ package com.niit.bej.c2s1MysqlPc1.repositry;
 
 import com.niit.bej.c2s1MysqlPc1.domain.Product;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends CrudRepository<Product, Integer> {
 
 }
